92fa11fdd0114f26a707a3f7bb6c8adfe637c8b5
Akash <akashorasad2000@gmail.com>
2020-07-16 00:12:24 +0530

    refactored project structure

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const express = require('express');
 const morgan = require('morgan')
 
 const globalErrorHandler = require('./controllers/errorController')
-const AppError = require('./utils/appError');
 
 const productsRouter = require('./routes/productRoutes');
-const usersRouter = require('./routes/userRoutes')
 
 const app = express();
 console.log(process.env.NODE_ENV)
@@ -17,15 +15,8 @@ app.use(express.json());
 
 
 app.use('/api/v1/products', productsRouter)
-app.use('/api/v1/users', usersRouter)
 
-app.all('*', (req, res, next) => {
-    // const err = new Error('cant find the defined route');
-    // err.status = 'fail';
-    // err.statusCode = 404;
 
-    next(new AppError('can\'t find the defined route', 404))
-})
 
 app.use(globalErrorHandler)
 
